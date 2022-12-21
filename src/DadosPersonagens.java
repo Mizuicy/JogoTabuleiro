145cb59ba48763ef33b1b93c3a174d9dfeb0bec5
CTT <gustavosilva90@gmail.com>
2022-12-21 13:51:57 -0300

    Fazendo um metodo contrutor

--- src/DadosPersonagens.java
+++ src/DadosPersonagens.java
@@ -2,22 +2,39 @@ import java.util.Random;
 
 class DadosPersongens {
     Random random = new Random();
-    int Hp;
-    int Atk;
-    int Def;
-    int Armdef;
-    int Armatk;
+    public int Hp;
+    public int Atk;
+    public int Def;
+    public int Armdef;
+    public int Psc;
     
-    public DadosPersongens(int hp, int atk, int def, int armdef, int armatk) {
-        Hp = hp;
-        Atk = atk;
-        Def = def;
-        Armdef = armdef;
-        Armatk = armatk;
-        
+
+    public DadosPersongens(Random random, int hp, int atk, int def, int armdef, int psc) {
+        this.random = random;
+        this.Hp = hp;
+        this.Atk = atk;
+        this.Def = def;
+        this.Armdef = armdef;
+        this.Psc = psc;
     }
+
+   
     public int D20(){
-        int d20 = random.nextInt(20);
+        int d20 = random.nextInt(0,21);
         return d20;
     }
+
+    public int D12(){
+        int d12 = random.nextInt(0 ,12);
+        return d12;
+    }
+
+    public int D8(){
+        int d8 = random.nextInt(0,8);
+        return d8;
+    }
+
+    public boolean isDead()  {
+        return this.Hp <= 0;
+}
 }
--- src/Jogo.java
+++ src/Jogo.java
@@ -1,25 +1,24 @@
 public class Jogo {
     public static void main(String[] args) throws Exception {
-        DadosPersongens humano = new DadosPersongens(0, 0, 0, 0, 0);
+        DadosPersongens humano = new DadosPersongens(null, 0, 0, 0, 0, 0);
         humano.Hp = 10;
         humano.Atk = 10;
         humano.Def = 10;
         humano.Armdef = humano.Def + 4;
-        humano.Armatk = humano.Atk + 2;
+        humano.Psc = 2;
 
-        DadosPersongens orc = new DadosPersongens(0, 0, 0, 0, 0);
+
+        DadosPersongens orc = new DadosPersongens(null, 0, 0, 0, 0, 0);
         orc.Hp = 8;
-        orc.Atk = 12;
         orc.Def = 10;
-        orc.Armatk = 8;
         
-        while(humano.Hp >= 0 && orc.Hp >= 0){
-            for(int i=0; i < humano.Hp; i++){
-                System.out.println("Primeiro rodada");
-                System.out.println(humano.D20());
+        while(!(humano.Hp <= 0) && !(orc.Hp != 0)){
 
+                System.out.println("rodada");
 
+            
             }
+            
         }
     }
-}
+
