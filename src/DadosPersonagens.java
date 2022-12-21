d7575b3dbece1faf32b597027907373b637efd6e
Gustavo_Oliveira_M <102737167+Mizuicy@users.noreply.github.com>
2022-12-20 23:11:14 -0300

    ATUALIZAÇÃO

--- src/DadosPersonagens.java
+++ src/DadosPersonagens.java
@@ -1,7 +1,23 @@
+import java.util.Random;
+
 class DadosPersongens {
+    Random random = new Random();
     int Hp;
     int Atk;
     int Def;
     int Armdef;
     int Armatk;
+    
+    public DadosPersongens(int hp, int atk, int def, int armdef, int armatk) {
+        Hp = hp;
+        Atk = atk;
+        Def = def;
+        Armdef = armdef;
+        Armatk = armatk;
+        
+    }
+    public int D20(){
+        int d20 = random.nextInt(20);
+        return d20;
+    }
 }
--- src/Jogo.java
+++ src/Jogo.java
@@ -1,18 +1,25 @@
 public class Jogo {
     public static void main(String[] args) throws Exception {
-        DadosPersongens humano = new DadosPersongens();
+        DadosPersongens humano = new DadosPersongens(0, 0, 0, 0, 0);
+        humano.Hp = 10;
         humano.Atk = 10;
         humano.Def = 10;
         humano.Armdef = humano.Def + 4;
         humano.Armatk = humano.Atk + 2;
 
-        DadosPersongens orc = new DadosPersongens();
+        DadosPersongens orc = new DadosPersongens(0, 0, 0, 0, 0);
+        orc.Hp = 8;
         orc.Atk = 12;
         orc.Def = 10;
         orc.Armatk = 8;
         
-        if (humano.Armatk >= orc.Def){
-            
+        while(humano.Hp >= 0 && orc.Hp >= 0){
+            for(int i=0; i < humano.Hp; i++){
+                System.out.println("Primeiro rodada");
+                System.out.println(humano.D20());
+
+
+            }
         }
     }
 }
